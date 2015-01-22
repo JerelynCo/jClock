bd78f4bfb640c58ef18378c89b6a6eea54313324
Ivan <marionltan@gmail.com>
2015-01-22 10:57:05 -0800

    added infinite loop

--- src/jClock/Main.java
+++ src/jClock/Main.java
@@ -2,19 +2,24 @@ package jClock;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
+	static Scanner scanner = new Scanner(System.in);//not using this yet
+	static String input = "";
 	public static void main(String[] args){
 		getTime();
 	}
 	
 	static void getTime(){
-		
-		//for(int i = 0; i<100; i++){	
+	input = "red";	
+		while(input.equals("end") == false ){	
 		    Date date = new Date();
 			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy h:mm:ss a");
 			String formattedDate = sdf.format(date);
 			System.out.println(formattedDate); // 12/01/2011 4:48:16 PM
-		//}
+			input = "asd";
+		}
+		
 	}	
 }
