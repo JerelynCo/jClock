d047da163427be70bd5cdc2ece793e9c12ba2bbd
JerelynCo <jerelynco@gmail.com>
2015-01-23 14:12:48 +0800

    Added javadoc and junit test case

--- Code/src/Clock.java
+++ Code/src/Clock.java
@@ -1,33 +1,51 @@
+/** 
+ * @author Jerelyn Co, Marion Ivan Tan
+ * @version 1.0
+ */
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Scanner;
+
+/*
+ * Class Clock will display the digital time.
+ */
 
 public class Clock {
-	public Clock(){
-		final int WIDTH = 480;
-		final int HEIGHT = 640;
-	}
-	
+	/*
+	 * @return String "Time: " and the formatted time from method digital time
+	 * @param args For console's use
+	 */
 	public static void main(String[] args) {
 		String last = "";
-		DateFormat df = new SimpleDateFormat("dd/MM/yy hh:mm:ss a");
+		DateFormat df = new SimpleDateFormat("hh:mm:ss a");
 		while(!stopTime()){
 			if(!digitalTime(df).equals(last)){
 				last = digitalTime(df);
-				System.out.println(last);
+				System.out.println("Time: " + last);
 			}
 		}
 	}
+	/*
+	 * @return String formatted time to display at main
+	 * @param df Format for the time
+	 */
 	public static String digitalTime(DateFormat df){
 		Calendar calobj = Calendar.getInstance();
 		return df.format(calobj.getTime());
 	}
-	
+	/*
+	 *stopTime will be used in the main to determine whether the application has to stop or not 
+	 * @return boolean to stop time
+	 */
 	public static boolean stopTime(){
 		return false;	
 	}
-	public static boolean alarm(){
+	/*
+	 *alarm will be used in the main to determine whether the application has to stop or not 
+	 * @return boolean true if the alarm is successfully set
+	 */
+	public static boolean setAlarm(){
 		return true;
 	}
 
